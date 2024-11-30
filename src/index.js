8187c344aeb0b3953bcc60ec426e15e6fe909930
GormanProg123 <gparfenchik2005@gmail.com>
2024-11-30 13:35:55 +0100

    Update 2

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import {Menu} from './components/menu/menu';
-import App from './components/camera/App';
 import reportWebVitals from './reportWebVitals';
+import App from './App'; // Импортируем App
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/app" element={<App />} />
+        {/* Основной маршрут, где отображается App */}
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
